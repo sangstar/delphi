7e32b43c64f42c6bff9ade49f79ea0c13168f8c4
Sanger Steel <sangersteel@gmail.com>
2024-08-12 08:58:00 -0400

    chore: Remove comments from boilerplate

--- setup.py
+++ setup.py
@@ -11,20 +11,13 @@ setup(
     url="https://github.com/sangstar/delphi",
     packages=find_packages(),
     install_requires=[
-        # List your project’s dependencies here
         "torch",
         "transformers",
     ],
     classifiers=[
-        # See https://pypi.org/classifiers/ for a full list of available classifiers
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.8",  # Specify Python version requirement
-    entry_points={
-        "console_scripts": [
-            "your_command=your_module:main_function",
-        ],
-    },
+    python_requires=">=3.8",
 )
